cd781dd5ab79089fdfe33a574265b3fe762c6785
Wayne Krug <wmkrug@gmail.com>
2014-10-18 14:56:56 -0500

    Added C solution to problem 1

---
+++ c/problem1.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main(const int argc, const char *argv[])
+{
+    int sum = 0;
+
+    for (int i = 1; i < 1000; ++i)
+    {
+        if ((i % 3) == 0 || (i % 5) == 0)
+        {
+            sum += i;
+        }
+    }
+
+    printf("The answer is %d\n", sum);
+
+    return 0;
+}
